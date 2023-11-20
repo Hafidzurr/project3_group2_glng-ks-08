c18f89382c362c9158aed497d664a8632fae35e8
Hafidzurr <hafidzurrohmansaifullah@gmail.com>
2023-11-20 18:59:31 +0700

    prepare deploy with update db.go

--- internal/config/db.go
+++ internal/config/db.go
@@ -5,10 +5,14 @@ import (
 	"github.com/Hafidzurr/project3_group2_glng-ks-08/internal/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	dsn := "host=localhost user=postgres dbname=Kanban_Board_db password=Hafidzurr1 sslmode=disable"
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		dsn = "host=localhost user=postgres dbname=Kanban_Board_db password=Hafidzurr1 sslmode=disable" // Lokal DB config
+	}
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Failed to connect to the database. Error:", err)
