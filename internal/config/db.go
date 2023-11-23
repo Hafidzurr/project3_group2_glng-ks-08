f3dc4e045a4b2992466968b17f040d9493db7e83
Hafidzurr <hafidzurrohmansaifullah@gmail.com>
2023-11-23 10:56:31 +0700

    update db railway

--- internal/config/db.go
+++ internal/config/db.go
@@ -8,10 +8,27 @@ import (
 	"os"
 )
 
+var (
+	host     = os.Getenv("PGHOST")
+	user     = os.Getenv("PGUSER")
+	password = os.Getenv("PGPASSWORD")
+	dbPort   = os.Getenv("PGPORT")
+	dbname   = os.Getenv("PGDATABASE")
+	// host       = "localhost"
+	// user       = "postgres"
+	// password   = "postgres890"
+	// dbPort     = "5432"
+	// dbname     = "mygram"
+	// debug_mode = os.Getenv("DEBUG_MODE")
+	DB  *gorm.DB
+	err error
+)
+
 func ConnectDB() (*gorm.DB, error) {
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
-		dsn = "host=localhost user=postgres dbname=Kanban_Board_db password=Hafidzurr1 sslmode=disable" // Lokal DB config
+		// dsn = "host=host user=postgres dbname=market password=Hafidzurr1 sslmode=disable" // Lokal DB config
+		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, dbPort)
 	}
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
